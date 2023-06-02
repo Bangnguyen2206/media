82704a35b085d07a260271058f46f1dc755b0b19
Bangnguyen2206 <tuyetbang0304@gmail.com>
2023-06-02 20:42:28 +0700

    Add schema user

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import express from "express";
+import { register } from "./controllers/auth.js";
 
 // *CONFIGURATION
 const __filename = fileURLToPath(import.meta.url);
@@ -49,6 +50,9 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+// ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
